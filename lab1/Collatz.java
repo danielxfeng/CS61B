1e16dd7252eb0e55fb987555648a8580b135aa03
Daniel F <126309503+DanielF2002@users.noreply.github.com>
2023-06-19 08:16:03 +0800

    Done with Lab1 Collatz number

--- lab1/Collatz.java
+++ lab1/Collatz.java
@@ -1,27 +1,32 @@
 /** Class that prints the Collatz sequence starting from a given number.
- *  @author YOUR NAME HERE
+ *  @author Daniel Feng
  */
 public class Collatz {
 
-    /** Buggy implementation of nextNumber! */
+    /**
+     * This method return the next Collatz number.
+     * @param n The start number of collatz number.
+     * @return The next Collatz number.
+     * */
     public static int nextNumber(int n) {
-        if (n  == 128) {
-            return 1;
-        } else if (n == 5) {
-            return 3 * n + 1;
-        } else {
-            return n * 2;
+        if (n % 2 == 0) {
+            return n / 2;
+        }
+        return n * 3 + 1;
+    }
+
+    /**
+     * This method print the Collatz number from 5
+     * @param args null
+     */
+    public static void main(String[] args) {
+        int n = 5; // the number in Lab requirement
+        System.out.println(n);
+        while (n != 1) {
+            n = nextNumber(n);
+            System.out.println(n);
         }
     }
 
-    public static void main(String[] args) {
-        int n = 5;
-        System.out.print(n + " ");
-        while (n != 1) {
-            n = nextNumber(n);
-            System.out.print(n + " ");
-        }
-        System.out.println();
-    }
 }
 
