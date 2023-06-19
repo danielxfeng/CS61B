4cdc07b15b77acc88cffcf4ad5f8c5e22282c338
Daniel F <126309503+DanielF2002@users.noreply.github.com>
2023-06-19 08:29:17 +0800

    Adjusted again for some strange white blanks

--- lab1/Collatz.java
+++ lab1/Collatz.java
@@ -21,7 +21,7 @@ public class Collatz {
      */
     public static void main(String[] args) {
         int n = 5; // the number in Lab requirement
-        System.out.print(n + " ");
+        System.out.print("          " + n + " ");
         while (n != 1) {
             n = nextNumber(n);
             System.out.print(n + " ");
